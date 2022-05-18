96a213c0fc6afb9f8b9b06b172e7b5c76c36d38f
Irwin Arruda <arruda.irwin@gmail.com>
2022-05-17 23:33:10 -0300

    ✨ Added svelte manually. Smui not working well

---
+++ games/src/App.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import Button, { Label } from '@smui/button';
+  import Card, { Content } from '@smui/card';
+  let count = 0;
+
+  function increment() {
+    count++;
+  }
+
+  function decrement() {
+    count--;
+  }
+</script>
+
+<div>
+  <Card>
+    <Content>
+      <h1 class="mdc-typography--headline2">SvelteJs Counter</h1>
+      <h2 class="mdc-typography--headline2">{count}</h2>
+      <Button color="secondary" variant="raised" on:click={decrement}><Label>Decrement</Label></Button>
+      <Button color="primary" variant="raised" on:click={increment}><Label>Increment</Label></Button>
+    </Content>
+  </Card>
+</div>
---
+++ games/webpack.config.js
@@ -0,0 +1,98 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const preprocess = require('svelte-preprocess');
+const TsConfig = require('./tsconfig.json');
+const PackageJson = require('./package.json');
+const path = require('path');
+
+// Dependencies from package.json
+const PackageJsonDeps = PackageJson.dependencies;
+
+// Uses tsconfig paths dynamicly to create webpack aliases
+const TsConfigPaths = Object.entries(TsConfig.compilerOptions.paths).reduce(
+  (prev, [key, value]) => ({
+    ...prev,
+    [key.replace('/*', '')]: path.resolve(value[0].replace('/*', '').replace('*', '')),
+  }),
+  {},
+);
+
+const mode = process.env.NODE_ENV || 'development';
+const prod = mode === 'production';
+
+/** @type {import('webpack').Configuration} */
+module.exports = {
+  mode: mode,
+  entry: {
+    main: path.join(__dirname, 'src', 'index.ts'),
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name]-[contenthash].js',
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      minify: true,
+      inject: 'body',
+      scriptLoading: 'blocking',
+      template: path.join(__dirname, 'public', 'index.html'),
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.[tj]s/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-typescript'],
+          },
+        },
+      },
+      {
+        test: /\.svelte$/,
+        use: {
+          loader: 'svelte-loader',
+          options: {
+            compilerOptions: {
+              dev: !prod,
+            },
+            emitCss: prod,
+            hotReload: !prod,
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        // required to prevent errors from Svelte on Webpack 5+
+        test: /node_modules\/svelte\/.*\.mjs$/,
+        resolve: {
+          fullySpecified: false,
+        },
+      },
+    ],
+  },
+  resolve: {
+    alias: TsConfigPaths,
+    extensions: ['.ts', '.js', '.svelte'],
+  },
+  devtool: 'source-map',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    port: 3003,
+    open: true,
+    historyApiFallback: true,
+    hot: true,
+    liveReload: true,
+  },
+};
